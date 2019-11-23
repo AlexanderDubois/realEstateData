13c751500e14f570a8a2cef577f9dcd4f7425382
Alexander Dubois <sander.dubois@outlook.com>
2019-11-23 12:35:28 +0100

    Initial Commit

---
+++ realEstateDataTests/realEstateDataTests.swift
@@ -0,0 +1,34 @@
+//
+//  realEstateDataTests.swift
+//  realEstateDataTests
+//
+//  Created by Alexander Dubois on 2019-11-23.
+//  Copyright © 2019 Youth Group. All rights reserved.
+//
+
+import XCTest
+@testable import realEstateData
+
+class realEstateDataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
